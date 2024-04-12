02060786cd39bd8f2ad4802f1b7d10919dde3a00
Fabian Gruenewald <f.grunewald@rug.nl>
2024-04-12 09:48:44 +0200

    add high level API

---
+++ cgsmiles/__init__.py
@@ -0,0 +1,7 @@
+"""
+CGsmiles: Coarse-Grained Smiles (CGsmiles) for representing abitrarily complex molecules using line notation.
+"""
+
+from .read_cgsmiles import read_cgsmiles
+from .read_fragments import read_fragments
+from .resolve import MoleculeResolver
