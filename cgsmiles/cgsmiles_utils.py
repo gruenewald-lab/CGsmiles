6a4951dc66f5a90b0ba588948abd1b3ab6a5ee41
Fabian Gruenewald <f.grunewald@rug.nl>
2024-07-24 11:15:26 +0200

    adjust open bonds function to optionally take target nodes

--- cgsmiles/cgsmiles_utils.py
+++ cgsmiles/cgsmiles_utils.py
@@ -16,14 +16,18 @@ def find_complementary_bonding_descriptor(bonding_descriptor):
         compl = bonding_descriptor
     return compl
 
-def find_open_bonds(molecule):
+def find_open_bonds(molecule, target_nodes=None):
     """
     Collect all nodes which have an open bonding descriptor and store
     them as keys with a list of nodes as values.
     """
+    if target_nodes is None:
+        target_nodes = list(molecule.nodes)
+
     open_bonds_by_descriptor = defaultdict(list)
     open_bonds = nx.get_node_attributes(molecule, 'bonding')
     for node, bonding_types in open_bonds.items():
-        for bonding_types in bonding_types:
-            open_bonds_by_descriptor[bonding_types].append(node)
+        if node in target_nodes:
+            for bonding_types in bonding_types:
+                open_bonds_by_descriptor[bonding_types].append(node)
     return open_bonds_by_descriptor
