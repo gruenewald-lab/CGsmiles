0eaccc66ff5cc8227b0502449df07f9cafdfaa3b
Fabian Gruenewald <f.grunewald@rug.nl>
2024-08-29 16:28:03 +0200

    expose sampler

--- cgsmiles/__init__.py
+++ cgsmiles/__init__.py
@@ -5,3 +5,4 @@ CGsmiles: Coarse-Grained Smiles (CGsmiles) for representing abitrarily complex m
 from .read_cgsmiles import read_cgsmiles
 from .read_fragments import read_fragments
 from .resolve import MoleculeResolver
+from .sample import MoleculeSampler
