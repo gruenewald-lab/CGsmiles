df6c8d89fecbfc199cf2cbde5b5170a1cf46b5d0
Fabian Gruenewald <f.grunewald@rug.nl>
2024-11-08 11:22:57 +0100

    implement dialects and annotations

---
+++ cgsmiles/dialects.py
@@ -0,0 +1,78 @@
+from inspect import signature, Signature, Parameter
+from functools import partial
+
+def check_and_cast_types(bound_args, signature):
+    for name, value in bound_args.arguments.items():
+        param = signature.parameters.get(name)
+        # Check if a type annotation is present
+        if param and param.annotation != Parameter.empty:
+            expected_type = param.annotation
+
+        # Attempt type casting if the value is not of the expected type
+        if not isinstance(value, expected_type):
+            try:
+               bound_args.arguments[name] = expected_type(value)
+            except (TypeError, ValueError):
+                raise TypeError(f"Argument '{name}' must be of type {expected_type.__name__}")
+    return bound_args
+
+def _parse_node(string_iteratable, dialect_signature):
+    """
+    This base function parsers a CGSmiles node. It must be
+    decorated with a signature which defines the dialect.
+    The dialect sets expected labels and default values of
+    a given node.
+    """
+    args_found = []
+    kwargs_found = {}
+    if len(string_iteratable) > 0:
+        elements = string_iteratable.split(';')
+        for entry in elements:
+            key_value = entry.split('=')
+            if len(key_value) == 1:
+                args_found.append(key_value[0])
+            else:
+                kwargs_found[key_value[0]] = key_value[1]
+
+    applied_labels = dialect_signature.bind(*args_found,
+                                            **kwargs_found)
+    applied_labels = check_and_cast_types(applied_labels,
+                                          dialect_signature)
+    applied_labels.apply_defaults()
+    return applied_labels.arguments
+
+def create_dialect(default_attributes):
+    """
+    Creates a signature of default attributes.
+    Note that the order of the entries in the dict
+    determines the order of the args accapted.
+    """
+    parameters = []
+    for argname, default_value in default_attributes.items():
+        arg_type = type(default_value)
+        parameters.append(Parameter(argname,
+                                    Parameter.POSITIONAL_OR_KEYWORD,
+                                    default=default_value,
+                                    annotation=arg_type))
+    sig = Signature(parameters)
+    return sig
+
+##########################################################
+#                   KNOWN DIALECTS                       #
+##########################################################
+# this one is for global use
+# it is the base CGSmiles dialect
+GRAPH_BASE = create_dialect({"fragname": "NaN",
+                             "c": 0.0,
+                             "w": 1.0})
+parse_graph_base_node = partial(_parse_node, dialect_signature=GRAPH_BASE)
+# this one is an internal fukery until the pysmiles
+# base parser is available
+# it just strips the kwargs from fragments before
+# they go to the respective parser
+# in case of cgsmiles fragments it is a bit doing
+# double the work
+fragment_base = create_dialect({"w": 1.0})
+print(GRAPH_BASE)
+print(fragment_base)
+_fragment_node_parser = partial(_parse_node, dialect_signature=fragment_base)
