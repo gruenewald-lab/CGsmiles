5cf9fe8e4515e698e75a53e00ec98ef471f61e55
Fabian Gruenewald <f.grunewald@rug.nl>
2025-01-07 11:31:31 +0100

    add version variable

--- cgsmiles/__init__.py
+++ cgsmiles/__init__.py
@@ -1,6 +1,8 @@
 """
 CGsmiles: Coarse-Grained Smiles (CGsmiles) for representing abitrarily complex molecules using line notation.
 """
+import pbr.version
+__version__ = pbr.version.VersionInfo('cgsmiles').release_string()
 
 from .read_cgsmiles import read_cgsmiles
 from .read_fragments import read_fragments
